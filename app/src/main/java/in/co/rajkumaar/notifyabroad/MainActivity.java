b942e0def24a1e5bae016774658c6cb8584efb6c
Rajkumar S <rajkumaar2304@icloud.com>
2022-09-21 16:33:31 -0700

    check permissions on activity load

--- app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
+++ app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
@@ -1,14 +1,38 @@
 package in.co.rajkumaar.notifyabroad;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.READ_CALL_LOG;
+import static android.Manifest.permission.READ_PHONE_STATE;
+import static android.Manifest.permission.RECEIVE_SMS;
+import static in.co.rajkumaar.notifyabroad.Utils.hasPermissions;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {RECEIVE_SMS, READ_PHONE_STATE, READ_CALL_LOG};
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "Permissions updated : " + Arrays.toString(permissions));
     }
 }
