7379302a5c45201876f8bf5dc04a9644cfd5cdd3
Rajkumar S <rajkumaar2304@icloud.com>
2022-09-21 20:44:30 -0700

    validate token before saving settings
    - replace bot URL with bot token

--- app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
+++ app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
@@ -5,19 +5,24 @@ import static android.Manifest.permission.READ_PHONE_STATE;
 import static android.Manifest.permission.RECEIVE_SMS;
 import static in.co.rajkumaar.notifyabroad.Utils.hasPermissions;
 
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView permissionsNotGranted;
     private LinearLayout settingsLayout;
-    private EditText botURL;
+    private EditText botToken;
     private CheckBox notifyCalls;
     private CheckBox notifySMS;
 
-    private final String TELEGRAM_BOT_URL = "telegram_bot_url";
+    private final String TELEGRAM_BOT_TOKEN = "telegram_bot_token";
     private final String NOTIFY_CALLS = "notify_calls";
     private final String NOTIFY_SMS = "notify_sms";
 
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         permissionsNotGranted = findViewById(R.id.permissions_not_granted);
         settingsLayout = findViewById(R.id.settings_layout);
         Button saveSettings = findViewById(R.id.saveSettings);
-        botURL = findViewById(R.id.telegramURL);
+        botToken = findViewById(R.id.telegramToken);
         notifyCalls = findViewById(R.id.notifyCalls);
         notifySMS = findViewById(R.id.notifySMS);
 
@@ -57,18 +62,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         saveSettings.setOnClickListener(view -> {
-            // TODO - Add bot URL validation by using getMe
-            sharedPreferencesEditor.putString(TELEGRAM_BOT_URL, botURL.getText().toString());
-            sharedPreferencesEditor.putBoolean(NOTIFY_CALLS, notifyCalls.isChecked());
-            sharedPreferencesEditor.putBoolean(NOTIFY_SMS, notifySMS.isChecked());
-            sharedPreferencesEditor.apply();
+            String enteredBotToken = botToken.getText().toString();
+            TelegramAPI api = new TelegramAPI(MainActivity.this, enteredBotToken);
+            ProgressDialog dialog =
+                    ProgressDialog.show(
+                            MainActivity.this,
+                            "",
+                            "Validating your token...",
+                            true
+                    );
+            api.getMe(new TelegramAPIResponse() {
+                @Override
+                public void onSuccess(JSONObject response) {
+                    dialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Token validated successfully", Toast.LENGTH_SHORT).show();
+                    sharedPreferencesEditor.putString(TELEGRAM_BOT_TOKEN, enteredBotToken);
+                    sharedPreferencesEditor.putBoolean(NOTIFY_CALLS, notifyCalls.isChecked());
+                    sharedPreferencesEditor.putBoolean(NOTIFY_SMS, notifySMS.isChecked());
+                    sharedPreferencesEditor.apply();
+                    Log.v("MainActivity", response.toString());
+                }
+
+                @Override
+                public void onFailure(Exception exception) {
+                    dialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Token seems invalid", Toast.LENGTH_SHORT).show();
+                    exception.printStackTrace();
+                }
+            });
         });
     }
 
     private void initSettingsLayout() {
         permissionsNotGranted.setVisibility(View.GONE);
         settingsLayout.setVisibility(View.VISIBLE);
-        botURL.setText(sharedPreferences.getString(TELEGRAM_BOT_URL, ""));
+        botToken.setText(sharedPreferences.getString(TELEGRAM_BOT_TOKEN, ""));
         notifyCalls.setChecked(sharedPreferences.getBoolean(NOTIFY_CALLS, false));
         notifySMS.setChecked(sharedPreferences.getBoolean(NOTIFY_SMS, false));
     }
