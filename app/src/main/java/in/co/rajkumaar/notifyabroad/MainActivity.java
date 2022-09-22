381c5d400f63a1f3dfff26e3d8441bcf683d8dd9
Rajkumar S <rajkumaar2304@icloud.com>
2022-09-21 19:28:05 -0700

    handle onRequestPermissions result

--- app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
+++ app/src/main/java/in/co/rajkumaar/notifyabroad/MainActivity.java
@@ -5,6 +5,7 @@ import static android.Manifest.permission.READ_PHONE_STATE;
 import static android.Manifest.permission.RECEIVE_SMS;
 import static in.co.rajkumaar.notifyabroad.Utils.hasPermissions;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,19 +21,20 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivity";
+    private TextView permissionsNotGranted;
+    private LinearLayout settingsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView permissionsNotGranted = findViewById(R.id.permissions_not_granted);
-        LinearLayout settingsLayout = findViewById(R.id.settings_layout);
+        permissionsNotGranted = findViewById(R.id.permissions_not_granted);
+        settingsLayout = findViewById(R.id.settings_layout);
 
         int PERMISSION_ALL = 1;
         String[] PERMISSIONS = {RECEIVE_SMS, READ_PHONE_STATE, READ_CALL_LOG};
         if (!hasPermissions(this, PERMISSIONS)) {
-            permissionsNotGranted.setVisibility(View.VISIBLE);
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         } else {
             permissionsNotGranted.setVisibility(View.GONE);
@@ -43,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d(TAG, "Permissions updated : " + Arrays.toString(permissions));
+        boolean allPermissionsGranted = true;
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                allPermissionsGranted = false;
+                break;
+            }
+        }
+        if (allPermissionsGranted) {
+            permissionsNotGranted.setVisibility(View.GONE);
+            settingsLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
