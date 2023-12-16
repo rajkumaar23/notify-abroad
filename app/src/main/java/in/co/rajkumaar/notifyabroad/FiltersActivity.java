6297dcb2ad0ce56e726fd0c10304edac64546cc0
Rajkumar <rajkumaar2304@icloud.com>
2023-12-15 21:48:24 -0700

    init sms filters

---
+++ app/src/main/java/in/co/rajkumaar/notifyabroad/FiltersActivity.java
@@ -0,0 +1,14 @@
+package in.co.rajkumaar.notifyabroad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiltersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
